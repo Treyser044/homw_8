183d361a226d5988e82261e4f2d0d153360bb318
Treyser044 <treyser044@gmail.com>
2025-07-08 09:33:58 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -139,10 +139,16 @@ console.log(`1 foydalanuvchi ismi ${users[0].name} \n  foydalanuvchi taxalusi ${
 
 
 let array = [2, 4, 9, 10, 45, 67, 8, 90];
-console.log(array, 'boshlanishdagi array'); 
+console.log(array, 'boshlanishdagi array');
 
-let juftSonlar = array.filter(function(son) {
-    return son % 2 === 0;
-});
+let juftSonlar = []; 
+
+for (let i = 0; i < array.length; i++) {
+    let son = array[i];
+    if (son % 2 === 0) {
+        juftSonlar.push(son); 
+    }
+}
 
 console.log(juftSonlar, 'juft sonlarga ajratilgan array');
+
